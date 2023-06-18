d9d2ff82eee1c60800b8b54b0106b7ec0c787398
GrusEton <navidben45@gmail.com>
2023-06-18 22:38:11 +0000

    Add new file for hostels

---
+++ controller/hostel.js
@@ -0,0 +1,23 @@
+const Hostel = require("../models/hostel");
+
+async function get_hostel(req, res) {
+    
+}
+
+async function get_hostels(req, res) {
+
+}
+
+async function add_hostel(req, res) {
+
+}
+
+async function update_hostel(req, res) {
+
+}
+
+async function delete_hostel(req, res) {
+    
+}
+
+module.exports = {get_hostel, get_hostels, add_hostel, update_hostel, delete_hostel};
