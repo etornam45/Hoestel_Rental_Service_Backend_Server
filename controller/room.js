220306426df3ce7ddf64bc54681fcf679afb11e2
GrusEton <navidben45@gmail.com>
2023-06-19 08:43:36 +0000

    Added function for Get single room in controller

--- controller/room.js
+++ controller/room.js
@@ -1,7 +1,17 @@
 const Room = require("../models/room");
 
 async function get_room(req, res) {
-    
+  await Room.findById(req.params.room_id)
+    .then((result) => {
+        if(result){
+            res.status().json(result)
+        }else{
+            res.sendStatus(404)
+        }
+    })
+    .catch((err) => {
+        res.status(500).json(err)
+    });
 }
 
 async function get_rooms(req, res) {
